3068c1b728a8c8a3136f786cd3c62ed132d47325
renxzen <renxzen@gmail.com>
2023-05-04 04:58:27 -0500

    added configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			screens: {
+				xs: "426px",
+				sm: "641px",
+				md: "769px",
+				lg: "1025px",
+				xl: "1281px",
+				"2xl": "1536px",
+			},
+			colors: {
+				coral: "#DC6453",
+			},
+			width: {
+				500: "32rem",
+				300: "19rem",
+			},
+			maxWidth: {
+				500: "32rem",
+			},
+		},
+	},
+	plugins: [],
 };
