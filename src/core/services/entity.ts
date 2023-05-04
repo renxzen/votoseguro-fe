75f62e2bbeee8f43d66c25cf515a7188bca51a10
renxzen <renxzen@gmail.com>
2023-05-04 17:32:10 -0500

    updated api url

--- src/core/services/entity.ts
+++ src/core/services/entity.ts
@@ -1,4 +1,4 @@
-const baseUrl = "https://4c27-2800-4b0-430b-f5a0-c835-b17f-e988-cc0c.ngrok-free.app";
+const baseUrl = "https://4c27-2800-4b0-430b-f5a0-c835-b17f-e988-cc0c.ngrok-free.app/api/v1.0";
 
 export const fetchEntities = async () =>
 	(await fetch(`${baseUrl}/entity`)).json();
--- src/core/services/user.ts
+++ src/core/services/user.ts
@@ -1,6 +1,6 @@
 import { LoginRequest, OtpRequest } from "../types/Requests";
 
-const baseUrl = "https://4c27-2800-4b0-430b-f5a0-c835-b17f-e988-cc0c.ngrok-free.app";
+const baseUrl = "https://4c27-2800-4b0-430b-f5a0-c835-b17f-e988-cc0c.ngrok-free.app/api/v1.0";
 
 export const fetchUser = async (request: LoginRequest) =>
 	(
