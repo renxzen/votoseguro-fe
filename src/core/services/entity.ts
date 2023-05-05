2b0880fb8bf833c15c3be8177cf4daad8b926325
renxzen <renxzen@gmail.com>
2023-05-05 08:20:55 -0500

    mocks implemented

--- src/core/services/entity.ts
+++ src/core/services/entity.ts
@@ -1,79 +1,10 @@
+import mockEntities from "~/core/mocks/entities.json";
+import mockCandidates from "~/core/mocks/candidates.json";
+
 const baseUrl =
 	"https://4c27-2800-4b0-430b-f5a0-c835-b17f-e988-cc0c.ngrok-free.app/api/v1.0";
-
-const mockEntities = {
-	count: 4,
-	next: null,
-	previous: null,
-	results: [
-		{
-			id: 1,
-			name: "Club de Regatas",
-			ruc: "10741222766",
-			description: "Votación del presidente del club",
-			image: "https://i.imgur.com/KRC0rzU.png",
-			address: "Jr. Vara de oro 193 4ta zona tahuantinsuyo",
-		},
-		{
-			id: 3,
-			name: "Lima Golf Club",
-			ruc: "12345678912",
-			description: "Votación del tesorero oficial.",
-			image: "https://i.imgur.com/lFPN11G.png",
-			address: "Av. Camino Real 770, San Isidro 15073",
-		},
-		{
-			id: 5,
-			name: "Club Lawn Tennis",
-			ruc: "2073067761",
-			description: "Votación para renovación de local.",
-			image: "https://i.imgur.com/ISvAF1g.png",
-			address: "Av. República de Chile 254, Jesús María 15072",
-		},
-		{
-			id: 4,
-			name: "Lima Cricket",
-			ruc: "1073067761",
-			description: "Repechaje de la división A2",
-			image: "https://i.imgur.com/kU3MCo9.png",
-			address: "Faustino Sánchez Carrión #200",
-		},
-	],
-};
 
 export const fetchEntities = async () =>
 	fetch(`${baseUrl}/entity`)
 		.then((response) => response.json())
 		.catch(() => mockEntities);
-
-/* export const fetchEntities = async () =>
-	await Promise.resolve(); */
-
-export const fetchCandidates = async () =>
-	await Promise.resolve({
-		count: 2,
-		next: null,
-		previous: null,
-		results: [
-			{
-				id: 1,
-				full_name: "Richard Cancino",
-				image:
-					"http://c5af-190-238-238-228.ngrok-free.app/media/candidates/None__RICHARD_CANCINO__292543349_462260685901694_6263011822499862470_n.jpeg",
-				description: "Alcalde guapo",
-				update_at: "2023-05-04",
-				number_voters: 3,
-				percentage_number_voters: "100.0%",
-			},
-			{
-				id: 3,
-				full_name: "Austin Ramirez",
-				image:
-					"http://c5af-190-238-238-228.ngrok-free.app/media/candidates/None__AUSTIN_RAMIREZ__292543349_462260685901694_6263011822499862470_n.jpeg",
-				description: "Proplayer",
-				update_at: "2023-05-04",
-				number_voters: 0,
-				percentage_number_voters: "0.0%",
-			},
-		],
-	});
--- src/core/services/user.ts
+++ src/core/services/user.ts
@@ -1,42 +1,24 @@
 import { LoginRequest, OtpRequest } from "../types/Requests";
+import mockUser from "~/core/mocks/user.json";
+import mockOtp from "~/core/mocks/otp.json";
 
-const baseUrl = "https://4c27-2800-4b0-430b-f5a0-c835-b17f-e988-cc0c.ngrok-free.app/api/v1.0";
+const baseUrl =
+	"https://4c27-2800-4b0-430b-f5a0-c835-b17f-e988-cc0c.ngrok-free.app/api/v1.0";
 
 export const fetchUser = async (request: LoginRequest) =>
-	(
-		await fetch(`${baseUrl}/user/login-voter/`, {
-			method: "POST",
-			body: JSON.stringify(request),
-			headers: { "Content-type": "application/json; charset=UTF-8" },
-		})
-	).json();
-
-/* export const fetchUser = async (request: LoginRequest) =>
-	await Promise.resolve({
-		status: "successful",
-		msg: "Vontante activo, falta OTP",
-		token: "57b34819f53986c6f08b5013ffe0e33b61e613c0",
-		details: {
-			full_name: "Richard Cancino",
-			dni: "23123123",
-		},
-	}); */
+	fetch(`${baseUrl}/user/login-voter/`, {
+		method: "POST",
+		body: JSON.stringify(request),
+		headers: { "Content-type": "application/json; charset=UTF-8" },
+	})
+		.then((response) => response.json())
+		.catch(() => mockUser);
 
 export const fetchOtp = async (request: OtpRequest) =>
-	(
-		await fetch(`${baseUrl}/user/validate-otp/`, {
-			method: "POST",
-			body: JSON.stringify(request),
-			headers: { "Content-type": "application/json; charset=UTF-8" },
-		})
-	).json();
-
-/* export const fetchOtp = async (request: OtpRequest) =>
-	await Promise.resolve({
-		status: "successful",
-		msg: "OTP validado",
-		details: {
-			token: "f419b99f5ae92d4873d54ed5183ecc9200f4c363",
-			code: "1234",
-		},
-	}); */
+	fetch(`${baseUrl}/user/validate-otp/`, {
+		method: "POST",
+		body: JSON.stringify(request),
+		headers: { "Content-type": "application/json; charset=UTF-8" },
+	})
+		.then((response) => response.json())
+		.catch(() => mockOtp);
