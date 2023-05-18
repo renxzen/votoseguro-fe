c3c08ff484cbbef611f2d51e422e4109c3991747
renxzen <renxzen@gmail.com>
2023-05-18 12:59:34 -0500

    added voting

--- src/core/services/entity.ts
+++ src/core/services/entity.ts
@@ -3,6 +3,7 @@ import mockEntities from "~/core/mocks/entities.json";
 import mockCandidates from "~/core/mocks/candidates.json";
 import { Entity } from "../types/Entity";
 import { Candidate } from "../types/Responses";
+import { VoteRequest } from "../types/Requests";
 
 export const fetchEntities = async (): Promise<Entity[]> =>
 	fetch(`${baseUrl}/entity/`)
@@ -15,3 +16,10 @@ export const fetchCandidates = async (entityId: string): Promise<Candidate[]> =>
 		.then((response) => response.json())
 		.then((response: { results: Candidate[] }) => response.results)
 		.catch(() => mockCandidates.results);
+
+export const submitVoteOnCandidate = async (request: VoteRequest) =>
+	fetch(`${baseUrl}/vote/choose-candidate/`, {
+		method: "POST",
+		body: JSON.stringify(request),
+		headers: { "Content-type": "application/json; charset=UTF-8" },
+	}).then((response) => response.json());
